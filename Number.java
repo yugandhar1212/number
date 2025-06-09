f30c1702882cadfeace84faeccee9840c304ac26
yugandhar1212 <yogiyogandhar05@gmail.comgit config --global user.gmail yogiyogandhar05@gmail.com>
2025-06-09 12:12:35 +0530

    msg

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number {
+    public static void main(String[] args) {
+        int a = 1;
+
+        if (a > 0) {
+            System.out.println("Number is positive");
+        } else if (a < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}
